b99952f17b9e00df0bcc10a192f7a1d4cd16794e
koungq <guu119@naver.com>
2024-07-05 02:05:11 +0900

    Feat: 코드 내 처리할 예외 타입

---
+++ src/main/java/leets/weeth/global/common/exception/ExceptionType.java
@@ -0,0 +1,27 @@
+package leets.weeth.global.common.exception;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExceptionType {
+    ENTITY_NOT_FOUND(HttpStatus.BAD_REQUEST, EntityNotFoundException.class),
+    ENTITY_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, EntityExistsException.class),
+    BUSINESS_LOGIC_ERROR(HttpStatus.BAD_REQUEST, BusinessLogicException.class);
+
+    private final HttpStatus httpStatus;
+    private final Class<? extends Exception> type;
+
+    public static Optional<ExceptionType> findException(Exception ex) {
+        return Arrays.stream(ExceptionType.values())
+                .filter(e -> e.getType().equals(ex.getClass())) // 클래스 타입
+                .findAny();
+    }
+}
