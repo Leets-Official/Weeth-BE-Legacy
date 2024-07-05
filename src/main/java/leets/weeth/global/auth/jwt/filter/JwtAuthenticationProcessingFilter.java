0a34c52e5076092c311dd84db73b4a4808ed16b2
koungq <guu119@naver.com>
2024-07-05 14:27:31 +0900

    Fix: 소셜 로그인 관련된 코드 삭제

--- src/main/java/leets/weeth/global/auth/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/leets/weeth/global/auth/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -80,9 +80,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     public void saveAuthentication(User myUser) {
         String password = myUser.getPassword();
-//        if (password == null) { // 소셜 로그인 유저의 비밀번호 임의로 설정 하여 소셜 로그인 유저도 인증 되도록 설정
-//            password = PasswordUtil.generateRandomPassword();
-//        }
 
         UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
                 .username(myUser.getUsername())
