294c9fab7099783b257efa08edf94150d690fe37
koungq <guu119@naver.com>
2024-07-07 18:04:01 +0900

    Feat: User 객체로 변환을 위한 MapStruct

---
+++ src/main/java/leets/weeth/domain/user/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package leets.weeth.domain.user.mapper;
+
+import leets.weeth.domain.user.dto.UserDTO;
+import leets.weeth.domain.user.entity.User;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+
+    UserMapper INStANCE = Mappers.getMapper(UserMapper.class);
+
+    // 수정: 모든 mapper 어노테이션 매핑 최소화
+    @Mappings({
+            @Mapping(target = "cardinals", expression = "java( java.util.List.of(dto.getCardinal()) )"),
+            @Mapping(target = "password", expression = "java( passwordEncoder.encode(dto.getPassword()) )")
+    })
+    User from(UserDTO.SignUp dto, @Context PasswordEncoder passwordEncoder);
+}
+
