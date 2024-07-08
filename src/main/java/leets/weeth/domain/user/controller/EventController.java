780f0e03e095b5595270cf67c0a9d670130c0080
hyxklee <ewgt1234@naver.com>
2024-07-08 16:05:07 +0900

    refactor: (EventController) 일정 상세 조회 api 구현

--- src/main/java/leets/weeth/domain/user/controller/EventController.java
+++ src/main/java/leets/weeth/domain/user/controller/EventController.java
@@ -1,13 +1,13 @@
 package leets.weeth.domain.user.controller;
 
 import leets.weeth.domain.user.dto.RequestEvent;
+import leets.weeth.domain.user.dto.ResponseEvent;
+import leets.weeth.domain.user.entity.Event;
 import leets.weeth.domain.user.service.EventService;
 import leets.weeth.global.common.response.CommonResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final EventService eventService;
 
+    // 일정 생성
     @PostMapping("/create")
     public CommonResponse<String> createEvent(@RequestBody RequestEvent requestEvent) {
         eventService.createEvent(requestEvent);
         return CommonResponse.createSuccess();
+    }
+
+    // 일정 상세 조회
+    @GetMapping("/{id}")
+    public CommonResponse<ResponseEvent> getEvent(@PathVariable Long id) {
+        ResponseEvent responseEvent = eventService.getEventById(id);
+        return CommonResponse.createSuccess(responseEvent);
     }
 }
