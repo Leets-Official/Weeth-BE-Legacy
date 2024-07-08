b3987e9d2b2ee39ab9bc9362d792c8d8baac0a9b
hyxklee <ewgt1234@naver.com>
2024-07-08 15:08:39 +0900

    feat: event 레포지토리 구현, 일정 조회 로직 구현

---
+++ src/main/java/leets/weeth/domain/user/repository/EventRepository.java
@@ -0,0 +1,15 @@
+package leets.weeth.domain.user.repository;
+
+import leets.weeth.domain.user.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByStartDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Optional<Event> findByEventId(Long eventId);
+}
