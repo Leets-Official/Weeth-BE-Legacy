a16a8b990a7da390673cc03037685a8ca970dcd8
hyxklee <ewgt1234@naver.com>
2024-07-08 16:16:03 +0900

    feat: (EventController) 기간 별 일정 조회 api 구현

--- src/main/java/leets/weeth/domain/user/controller/EventController.java
+++ src/main/java/leets/weeth/domain/user/controller/EventController.java
@@ -7,7 +7,11 @@ import leets.weeth.domain.user.service.EventService;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +31,15 @@ public class EventController {
     public CommonResponse<ResponseEvent> getEvent(@PathVariable Long id) {
         ResponseEvent responseEvent = eventService.getEventById(id);
         return CommonResponse.createSuccess(responseEvent);
+    }
+
+    // 기간 별 일정 조회
+    @GetMapping("")
+    public CommonResponse<List<ResponseEvent>> getEvents(
+            @RequestParam(name = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(name = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
+
+        List<ResponseEvent> responseEvents = eventService.getEventsBetweenDate(startDate, endDate);
+        return CommonResponse.createSuccess(responseEvents);
     }
 }
