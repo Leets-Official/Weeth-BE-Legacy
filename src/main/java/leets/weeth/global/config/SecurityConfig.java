83f771da2f78447dea14672f1e8f8a09786ce013
hyxklee <ewgt1234@naver.com>
2024-07-08 16:50:32 +0900

    refactor: events 관련 경로 주석 처리

--- src/main/java/leets/weeth/global/config/SecurityConfig.java
+++ src/main/java/leets/weeth/global/config/SecurityConfig.java
@@ -55,6 +55,8 @@ public class SecurityConfig {
                         authorize ->
                                 authorize
                                         .requestMatchers("/users/apply").permitAll()
+                                        //일정 관련 경로 권한 설정
+                                       // .requestMatchers("/events/**").permitAll()
                                         // 스웨거 경로
                                         .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger/**").permitAll()
                                         .anyRequest().authenticated()
