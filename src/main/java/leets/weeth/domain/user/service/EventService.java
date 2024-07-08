88be70c89c744bc4e5a301b571ba63059821e130
hyxklee <ewgt1234@naver.com>
2024-07-08 16:15:47 +0900

    feat: (EventService) 기간 별 일정 조회 로직 구현

--- src/main/java/leets/weeth/domain/user/service/EventService.java
+++ src/main/java/leets/weeth/domain/user/service/EventService.java
@@ -10,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class EventService {
@@ -29,5 +33,13 @@ public class EventService {
     public ResponseEvent getEventById(Long id) {
         Event event = eventRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("id에 해당하는 일정이 존재하지 않습니다"));
         return eventMapper.toDto(event);
+    }
+
+    // 기간 별 일정 조회
+    public List<ResponseEvent> getEventsBetweenDate(LocalDateTime startDate, LocalDateTime endDate) {
+        List<Event> events = eventRepository.findByStartDateTimeBetween(startDate, endDate);
+        return events.stream()
+                .map(eventMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
