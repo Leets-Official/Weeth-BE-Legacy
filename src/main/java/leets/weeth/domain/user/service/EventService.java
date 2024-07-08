de5882a6195e03dc2340aa13363ab8ea3f1051e8
hyxklee <ewgt1234@naver.com>
2024-07-08 15:38:12 +0900

    feat: (EventService) 이벤트 저장 로직 구현

---
+++ src/main/java/leets/weeth/domain/user/service/EventService.java
@@ -0,0 +1,25 @@
+package leets.weeth.domain.user.service;
+
+import leets.weeth.domain.user.dto.RequestEvent;
+import leets.weeth.domain.user.entity.Event;
+import leets.weeth.domain.user.mapper.EventMapper;
+import leets.weeth.domain.user.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    private final EventMapper eventMapper;
+
+    @Transactional
+    public void saveEvent(RequestEvent requestEvent) {
+        Event event = eventMapper.fromDto(requestEvent);
+        eventRepository.save(event);
+    }
+
+
+}
