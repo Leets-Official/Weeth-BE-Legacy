f0143f95eb056368dbb9f0029892cebb9e45bb23
hyxklee <ewgt1234@naver.com>
2024-07-09 22:39:17 +0900

    feat: 요청 dto에 빌더 패턴 적용

--- src/main/java/leets/weeth/domain/event/dto/RequestEvent.java
+++ src/main/java/leets/weeth/domain/event/dto/RequestEvent.java
@@ -2,9 +2,11 @@ package leets.weeth.domain.event.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record RequestEvent (
      @NotBlank String title,
      @NotBlank String content,
