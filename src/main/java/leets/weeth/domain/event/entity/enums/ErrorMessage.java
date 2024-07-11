a9e239b854bb0efcf6435807854cc1628077848d
hyxklee <ewgt1234@naver.com>
2024-07-11 22:55:21 +0900

    feat: 에러 메시지 추가

--- src/main/java/leets/weeth/domain/event/entity/enums/ErrorMessage.java
+++ src/main/java/leets/weeth/domain/event/entity/enums/ErrorMessage.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessage {
 
-    EVENT_NOT_FOUND("id에 해당하는 일정이 존재하지 않습니다.");
-
+    EVENT_NOT_FOUND("id에 해당하는 일정이 존재하지 않습니다."),
+    USER_NOT_FOUND("존재하지 않는 사용자입니다."),
+    USER_NOT_MATCH("일정과 사용자가 일치하지 않습니다.");
     private final String message;
 }
