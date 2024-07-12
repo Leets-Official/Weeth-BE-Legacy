2a139898427ac5d9d4d5fc9c88debc9af9a19458
huncozyboy <leejh5838@gmail.com>
2024-07-12 23:47:25 +0900

    Feat: 출석코드 관련 예외처리

---
+++ src/main/java/leets/weeth/domain/attendance/exception/AttendanceCodeException.java
@@ -0,0 +1,7 @@
+package leets.weeth.domain.attendance.exception;
+
+public class AttendanceCodeException extends RuntimeException {
+    public AttendanceCodeException(String message) {
+        super(message);
+    }
+}
