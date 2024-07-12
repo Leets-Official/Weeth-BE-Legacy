5e4f20cc658508203f435129531663f6fd7294e7
huncozyboy <leejh5838@gmail.com>
2024-07-12 23:49:16 +0900

    Refactor: 출석관련 오류 수정

--- src/main/java/leets/weeth/domain/attendance/service/AttendanceService.java
+++ src/main/java/leets/weeth/domain/attendance/service/AttendanceService.java
@@ -4,6 +4,7 @@ import jakarta.validation.ValidationException;
 import leets.weeth.domain.attendance.dto.RequestAttendance;
 import leets.weeth.domain.attendance.entity.Attendance;
 import leets.weeth.domain.attendance.entity.AttendanceCode;
+import leets.weeth.domain.attendance.entity.enums.Week;
 import leets.weeth.domain.user.entity.User;
 import leets.weeth.domain.user.repository.UserRepository;
 import leets.weeth.domain.attendance.repository.AttendanceRepository;
@@ -30,19 +31,19 @@ public class AttendanceService {
     public void checkInAttendance(RequestAttendance requestAttendance, String currentEmail) {
         Optional<AttendanceCode> optionalCode = attendanceCodeRepository.findById(requestAttendance.getAttendanceCodeId());
         if (optionalCode.isPresent()) {
-            AttendanceCode attendanceCode = optionalCode.get(); //일치하는지 확인
+            AttendanceCode attendanceCode = optionalCode.get(); // 일치하는지 확인
             if (Objects.equals(attendanceCode.getAttendanceCode(), requestAttendance.getAttendanceCode())) {
-                if (attendanceCode.getExpirationTime().isBefore(LocalDateTime.now())) {  //코드 만료시
+                if (attendanceCode.getExpirationTime().isBefore(LocalDateTime.now())) {  // 코드 만료 시
                     throw new AttendanceCodeException("출석 코드가 유효기간이 지났습니다.");
                 }
 
-                User user = userRepository.findByEmail(currentEmail)//이메일로 확인?
+                User user = userRepository.findByEmail(currentEmail) // 이메일로 확인
                         .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
                 Attendance attendance = new Attendance();
                 attendance.setAttend(requestAttendance.isAttend());
                 attendance.setUser(user);
-                attendance.setWeek(attendanceCode.getWeek());
+                attendance.setWeek(attendanceCode.getWeek()); // Week 타입으로 설정
                 attendanceRepository.save(attendance);
 
                 attendanceStatisticsService.updateAttendanceRate(user.getId());
