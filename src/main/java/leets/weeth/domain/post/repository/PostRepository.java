746ae474e9b60f887aa5bc9f97a82aaecbe20284
jj <syeonie0526@gmail.com>
2024-07-12 13:55:31 +0900

    Refactor: 사용하지 않는 임포트 삭제

--- src/main/java/leets/weeth/domain/post/repository/PostRepository.java
+++ src/main/java/leets/weeth/domain/post/repository/PostRepository.java
@@ -1,11 +1,11 @@
 package leets.weeth.domain.post.repository;
 
 import leets.weeth.domain.post.entity.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     @Override
     ArrayList<Post> findAll();
 }
