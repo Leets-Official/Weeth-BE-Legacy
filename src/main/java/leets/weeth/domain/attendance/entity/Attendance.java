910c0038f44d8bb76430abd49cd140576265ecb7
huncozyboy <leejh5838@gmail.com>
2024-07-12 23:17:12 +0900

    Feat: 기본 출석 엔티티 생성

---
+++ src/main/java/leets/weeth/domain/attendance/entity/Attendance.java
@@ -0,0 +1,25 @@
+package leets.weeth.domain.attendance.entity;
+
+import jakarta.persistence.*;
+import leets.weeth.domain.attendance.entity.enums.Week;
+import leets.weeth.domain.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean isAttend;
+
+    @Enumerated(EnumType.STRING)
+    private Week week;
+}
