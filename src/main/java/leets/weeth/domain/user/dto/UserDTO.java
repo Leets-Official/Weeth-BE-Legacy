4413d1bb1d637ab6fccc016808070d324a64302f
koungq <guu119@naver.com>
2024-07-15 23:22:59 +0900

    Feat: @CheckPassword를 통한 비밀번호 일치 여부 검사

--- src/main/java/leets/weeth/domain/user/dto/UserDTO.java
+++ src/main/java/leets/weeth/domain/user/dto/UserDTO.java
@@ -3,6 +3,7 @@ package leets.weeth.domain.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import leets.weeth.domain.user.annotation.CheckPassword;
 import leets.weeth.domain.user.entity.enums.Department;
 import leets.weeth.domain.user.entity.enums.Position;
 
@@ -12,8 +13,9 @@ public class UserDTO {
         @NotBlank        String name,
         @Email @NotBlank String email,
         @NotBlank        String password,
+        @NotBlank @CheckPassword String passwordConfirm,
         @NotBlank        String studentId,
-        @NotBlank        String phoneNumber,
+        @NotBlank        String tel,
         @NotNull         Position position,
         @NotNull         Department department,
         @NotNull         Integer cardinal
@@ -23,6 +25,7 @@ public class UserDTO {
             Integer id,
             String name,
             String studentId,
+            String tel,
             Department department,
             String email,
             Integer cardinal,
