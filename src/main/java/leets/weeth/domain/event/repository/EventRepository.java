e3fe34fe1369ea95f64e7feb1f8b91ec90bf38bd
hyxklee <ewgt1234@naver.com>
2024-07-16 23:44:36 +0900

    feat: 쿼리 줄이기 위한 노력

--- src/main/java/leets/weeth/domain/event/repository/EventRepository.java
+++ src/main/java/leets/weeth/domain/event/repository/EventRepository.java
@@ -13,8 +13,12 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByStartDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT e FROM Event e JOIN e.eventCalendars ec WHERE ec.calendar.id = :calendarId")
+    @Query("SELECT e FROM Event e JOIN FETCH e.user JOIN e.eventCalendars ec WHERE ec.calendar.id = :calendarId")
     List<Event> findByCalendarId(@Param("calendarId") Long calendarId);
+
+    // 좀 더 알아보고 쿼리 최적화하기
+    @Query("SELECT e FROM Event e JOIN FETCH e.user WHERE YEAR(e.startDateTime) = :year")
+    List<Event> findAllByYear(@Param("year") int year);
 
     // 유저가 작성한 일정 리스트 반환. 필요하다면 사용
 //    Optional<Event> findByIdAndUserId(Long eventId, Long userId);
