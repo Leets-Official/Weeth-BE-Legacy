71c1209f384f58b088306f06a5a048513780e84d
hyxklee <ewgt1234@naver.com>
2024-07-16 23:45:00 +0900

    feat: 년도별 조회 로직 구현

--- src/main/java/leets/weeth/domain/calendar/service/CalendarService.java
+++ src/main/java/leets/weeth/domain/calendar/service/CalendarService.java
@@ -12,8 +12,8 @@ import leets.weeth.domain.event.repository.EventRepository;
 import leets.weeth.global.common.error.exception.custom.BusinessLogicException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -44,23 +44,32 @@ public class CalendarService {
                     .toList());
     }
 
-    // 캘린더 조회 (년별)
+//    // 캘린더 조회 (년별)
+//    public ResponseYearCalendar getDataYear(int year) throws BusinessLogicException {
+//        //년도에 맞는 캘린더 반환, 해당 아이디로 이벤트_캘린더 조회 -> 이벤트 반환
+//        List<Calendar> calendars = calendarRepository.findByYear(year);
+//        List<Event> events = new ArrayList<>();
+//        for(Long calendarId: calendars.stream().map(Calendar::getId).toList()){
+//            List<Event> eventList = eventRepository.findByCalendarId(calendarId);
+//            events.addAll(eventList);
+//        }
+//
+//        Map<Integer, List<ResponseEvent>> eventsByMonth = events.stream()
+//                .sorted((e1, e2) -> e1.getStartDateTime().compareTo(e2.getStartDateTime()))
+//                .map(eventMapper::toDto)
+//                .collect(Collectors.groupingBy(responseEvent -> responseEvent.startDateTime().getMonthValue()));
+//        return new ResponseYearCalendar(year, eventsByMonth);
+//    }
+
+    @Transactional(readOnly = true)
     public ResponseYearCalendar getDataYear(int year) throws BusinessLogicException {
-        // 러프하게 년도 시작과 끝에서 events와 notices를 조회하고 정렬해서 DTO에 담아서 반환
-        LocalDateTime start = LocalDateTime.of(year, 1, 1, 0, 0);
-        LocalDateTime end = LocalDateTime.of(year, 12, 31, 23, 59);
-        List<Event> events = eventRepository.findByStartDateTimeBetween(start, end);
+        List<Event> events = eventRepository.findAllByYear(year);
 
         Map<Integer, List<ResponseEvent>> eventsByMonth = events.stream()
                 .sorted((e1, e2) -> e1.getStartDateTime().compareTo(e2.getStartDateTime()))
                 .map(eventMapper::toDto)
                 .collect(Collectors.groupingBy(responseEvent -> responseEvent.startDateTime().getMonthValue()));
 
-        // 공지사항도 created_at 날짜를 이용해 해당 년도의 공지를 가져와서 필요하면 정렬해서 보내주면 될 듯
         return new ResponseYearCalendar(year, eventsByMonth);
-
-        // 해당 년도의 캘린더 테이블을 돌아 ID를 구해서 해당 ID와 매칭되는 events와 notices들을 조회해서 정렬해서 DTO에 담아서 반환 -> 좀 비효율 적인거 같은데
-
     }
-
 }
