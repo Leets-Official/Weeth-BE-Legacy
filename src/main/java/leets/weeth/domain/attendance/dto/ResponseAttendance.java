7333ef70e3fa77e73fd8ee74a12b3eaac3159670
huncozyboy <leejh5838@gmail.com>
2024-07-17 22:10:39 +0900

    Refactor: 출석 조회 로직 추가(출석 상세정보 조회)

--- src/main/java/leets/weeth/domain/attendance/dto/ResponseAttendance.java
+++ src/main/java/leets/weeth/domain/attendance/dto/ResponseAttendance.java
@@ -1,22 +1,35 @@
 package leets.weeth.domain.attendance.dto;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+
 @Getter
 @Builder
 public class ResponseAttendance {
-    private String scheduleTitle; //일정 제목
-    private String scheduleDateTime; //날짜, 시간
-    private String scheduleLocation; //장소
-    private double attendanceRate; //출석률
-    private double absenceRate; //결석률
+    private String scheduleTitle; // 일정 제목
+    private String scheduleDateTime; // 날짜, 시간
+    private String scheduleLocation; // 장소
+    private double attendanceRate; // 출석률
+    private double absenceRate; // 결석률
+    private String weekStartDate; // 주차 시작 날짜
+    private String weekEndDate; // 주차 끝나는 날짜
 
-    public static ResponseAttendance create(String scheduleTitle, String scheduleDateTime, String scheduleLocation, double attendanceRate, double absenceRate) {
+    public static ResponseAttendance create(
+            String title,
+            String dateTime,
+            String location,
+            double attendanceRate,
+            double absenceRate,
+            String weekStartDate,
+            String weekEndDate) {
         return ResponseAttendance.builder()
-                .scheduleTitle(scheduleTitle)
-                .scheduleDateTime(scheduleDateTime)
-                .scheduleLocation(scheduleLocation)
+                .scheduleTitle(title)
+                .scheduleDateTime(dateTime)
+                .scheduleLocation(location)
                 .attendanceRate(attendanceRate)
                 .absenceRate(absenceRate)
+                .weekStartDate(weekStartDate)
+                .weekEndDate(weekEndDate)
                 .build();
     }
 }
