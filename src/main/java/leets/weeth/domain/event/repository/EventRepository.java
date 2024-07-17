fd136058ffca35d9a65495570dfdeaea26bbfc5f
hyxklee <ewgt1234@naver.com>
2024-07-17 11:16:41 +0900

    refactor: 사용하지 않는 쿼리 삭제

--- src/main/java/leets/weeth/domain/event/repository/EventRepository.java
+++ src/main/java/leets/weeth/domain/event/repository/EventRepository.java
@@ -16,10 +16,4 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT e FROM Event e JOIN FETCH e.user JOIN e.eventCalendars ec WHERE ec.calendar.id = :calendarId")
     List<Event> findByCalendarId(@Param("calendarId") Long calendarId);
 
-    // 좀 더 알아보고 쿼리 최적화하기
-    @Query("SELECT e FROM Event e JOIN FETCH e.user WHERE YEAR(e.startDateTime) = :year")
-    List<Event> findAllByYear(@Param("year") int year);
-
-    // 유저가 작성한 일정 리스트 반환. 필요하다면 사용
-//    Optional<Event> findByIdAndUserId(Long eventId, Long userId);
 }
