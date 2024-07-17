93ac22fa7a4b6629d77f827768b63300bdc211f6
hyxklee <ewgt1234@naver.com>
2024-07-17 13:08:49 +0900

    feat: eventId 조회 로직 추가

--- src/main/java/leets/weeth/domain/calendar/repository/EventCalendarRepository.java
+++ src/main/java/leets/weeth/domain/calendar/repository/EventCalendarRepository.java
@@ -12,4 +12,8 @@ public interface EventCalendarRepository extends JpaRepository<EventCalendar, Lo
     @Query("SELECT ec FROM EventCalendar ec JOIN FETCH ec.event e JOIN FETCH e.user WHERE ec.calendar.id IN :calendarIds")
     List<EventCalendar> findByCalendarIds(@Param("calendarIds") List<Long> calendarIds);
 
+    @Query("SELECT ec FROM EventCalendar ec JOIN FETCH ec.event e JOIN FETCH e.user WHERE ec.event.id= :eventId")
+    List<EventCalendar> findByEventId(@Param("eventId") Long eventId);
+
+    void deleteByEventId(Long eventId);
 }
