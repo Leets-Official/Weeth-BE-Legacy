d12c92595f0789e7ccecdd81b357d13ac598775f
huncozyboy <leejh5838@gmail.com>
2024-07-17 15:43:46 +0900

    Refactor: 엔티티 세터 삭제 후 빌더 어노테이션 적용

--- src/main/java/leets/weeth/domain/attendance/entity/Attendance.java
+++ src/main/java/leets/weeth/domain/attendance/entity/Attendance.java
@@ -3,23 +3,34 @@ package leets.weeth.domain.attendance.entity;
 import jakarta.persistence.*;
 import leets.weeth.domain.attendance.entity.enums.Week;
 import leets.weeth.domain.user.entity.User;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
+@NoArgsConstructor
 public class Attendance {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long attendanceId;;
+
+    private boolean isAttend;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    private boolean isAttend;
-    private String attendanceCode;
     @Enumerated(EnumType.STRING)
     private Week week;
+
+    @Builder
+    public Attendance(Long attendanceId, User user, Week week) {
+        this.attendanceId = attendanceId;
+        this.isAttend = isAttend();
+        this.user = user;
+        this.week = week;
+    }
 }
