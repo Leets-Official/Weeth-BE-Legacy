1ab6c4154c5ffd44ce23c857a4152a1146b99752
hyxklee <ewgt1234@naver.com>
2024-07-17 11:16:20 +0900

    refactor: 사용하지 않는 쿼리 삭제

--- src/main/java/leets/weeth/domain/calendar/repository/EventCalendarRepository.java
+++ src/main/java/leets/weeth/domain/calendar/repository/EventCalendarRepository.java
@@ -2,6 +2,14 @@ package leets.weeth.domain.calendar.repository;
 
 import leets.weeth.domain.calendar.entity.EventCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventCalendarRepository extends JpaRepository<EventCalendar, Long> {
+
+    @Query("SELECT ec FROM EventCalendar ec JOIN FETCH ec.event e JOIN FETCH e.user WHERE ec.calendar.id IN :calendarIds")
+    List<EventCalendar> findByCalendarIds(@Param("calendarIds") List<Long> calendarIds);
+
 }
