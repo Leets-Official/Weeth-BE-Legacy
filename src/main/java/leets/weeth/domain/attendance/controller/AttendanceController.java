95b626c1ecd6b276e625ff701ea8dc519b275f12
huncozyboy <leejh5838@gmail.com>
2024-07-17 14:22:33 +0900

    Refactor: user 조회방식 변경

--- src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
+++ src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
@@ -3,10 +3,14 @@ package leets.weeth.domain.attendance.controller;
 import leets.weeth.domain.attendance.dto.RequestAttendance;
 import leets.weeth.domain.attendance.dto.ResponseAttendance;
 import leets.weeth.domain.attendance.service.AttendanceService;
+import leets.weeth.global.auth.annotation.CurrentUser;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/attendance")
@@ -16,8 +20,8 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
 
     @PostMapping
-    public CommonResponse<ResponseAttendance> recordAttendance(@RequestBody @Validated RequestAttendance requestDto) {
-        ResponseAttendance responseDto = attendanceService.recordAttendance(requestDto);
+    public CommonResponse<ResponseAttendance> recordAttendance(@RequestBody @Validated RequestAttendance requestDto, @CurrentUser Long userId) {
+        ResponseAttendance responseDto = attendanceService.recordAttendance(requestDto, userId);
         return CommonResponse.createSuccess(responseDto);
     }
 }
