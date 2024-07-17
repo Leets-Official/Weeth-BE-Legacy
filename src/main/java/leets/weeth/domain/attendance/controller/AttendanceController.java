3838f3d434918c4813d6581e930539a90390fe31
huncozyboy <leejh5838@gmail.com>
2024-07-17 19:00:33 +0900

    Refactor: 스웨거 api 관련 메세지 수정

--- src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
+++ src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
@@ -1,17 +1,18 @@
 package leets.weeth.domain.attendance.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.attendance.dto.RequestAttendance;
 import leets.weeth.domain.attendance.dto.ResponseAttendance;
 import leets.weeth.domain.attendance.service.AttendanceService;
-import leets.weeth.global.auth.annotation.CurrentUser;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
+@Tag(name = "AttendanceController", description = "출석 관련 API")
 @RestController
 @RequestMapping("/attendance")
 @RequiredArgsConstructor
@@ -19,9 +20,13 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    @PostMapping
-    public CommonResponse<ResponseAttendance> recordAttendance(@RequestBody @Validated RequestAttendance requestDto, @CurrentUser Long userId) {
-        ResponseAttendance responseDto = attendanceService.recordAttendance(requestDto, userId);
-        return CommonResponse.createSuccess(responseDto);
+    @Operation(summary = "출석 체크", description = "사용자가 출석 코드를 입력하여 출석합니다.")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @PostMapping("/check-in")
+    public CommonResponse<ResponseAttendance> recordAttendance(
+            @RequestBody @Valid RequestAttendance requestDto,
+            @AuthenticationPrincipal Long user) {
+        ResponseAttendance response = attendanceService.recordAttendance(requestDto, user);
+        return CommonResponse.createSuccess(response);
     }
 }
