4c3224222b74682e2e42a0df852a0ef744b645c0
hyxklee <ewgt1234@naver.com>
2024-07-18 10:45:46 +0900

    feat: 쿼리 수정

--- src/main/java/leets/weeth/domain/event/repository/EventRepository.java
+++ src/main/java/leets/weeth/domain/event/repository/EventRepository.java
@@ -12,14 +12,14 @@ import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     // 기간 내의 모든 일정 반환
-    @Query("SELECT e FROM Event e WHERE e.startDateTime BETWEEN :startDate AND :endDate OR e.endDateTime BETWEEN :startDate AND :endDate")
+    @Query("SELECT e FROM Event e JOIN FETCH e.user WHERE e.startDateTime BETWEEN :startDate AND :endDate OR e.endDateTime BETWEEN :startDate AND :endDate")
     List<Event> findByStartDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
 
-    // status에 맞는 일정 반환
-    @Query("SELECT e FROM Event e WHERE e.id = :id AND e.status = :status")
+    // status에 맞는 일정 한 개 반환
+    @Query("SELECT e FROM Event e JOIN FETCH e.user WHERE e.id = :id AND e.status = :status")
     Optional<Event> findByIdAndStatus(@Param("id") Long id, @Param("status") Status status);
 
-    // attandance 용 하나
-//    @Query("SELECT e FROM Event e WHERE e.status = :ATTENDANCE")
-//    List<Event> findAllByStatus(@Param("status") Status status);
+    // status에 맞는 일정 모두 반환
+    @Query("SELECT e FROM Event e JOIN FETCH e.user WHERE e.status = :status")
+    List<Event> findAllByStatus(@Param("status") Status status);
 }
