4fd5a0541df223362cbebd85f58f98bf51986e6c
hyxklee <ewgt1234@naver.com>
2024-07-18 10:45:20 +0900

    feat: mapper 수정

--- src/main/java/leets/weeth/domain/notice/mapper/NoticeMapper.java
+++ src/main/java/leets/weeth/domain/notice/mapper/NoticeMapper.java
@@ -1,23 +1,21 @@
-package leets.weeth.domain.event.mapper;
+package leets.weeth.domain.notice.mapper;
 
-import leets.weeth.domain.event.dto.ResponseEvent;
 import leets.weeth.domain.event.entity.Event;
+import leets.weeth.domain.notice.dto.ResponseNotice;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
-public interface EventMapper {
-    EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
-
-//    Event fromDto(RequestEvent requestEvent);
+public interface NoticeMapper {
+    NoticeMapper INSTANCE = Mappers.getMapper(NoticeMapper.class);
 
     @Mappings({
             @Mapping(source = "user.name", target = "userName"),
             @Mapping(source = "createdAt", target = "created_at"),
             @Mapping(source = "modifiedAt", target = "modified_at"),
     })
-    ResponseEvent toDto(Event event);
+    ResponseNotice toDto(Event event);
 
 }
