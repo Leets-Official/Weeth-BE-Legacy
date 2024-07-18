406bf4006fccd137513a3e90e264d7b2a1137e31
hyxklee <ewgt1234@naver.com>
2024-07-18 11:36:45 +0900

    feat: 모든 공지사항 조회 구현

--- src/main/java/leets/weeth/domain/notice/controller/NoticeController.java
+++ src/main/java/leets/weeth/domain/notice/controller/NoticeController.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static leets.weeth.domain.event.entity.enums.ResponseMessage.*;
 
 @Tag(name = "NoticeController", description = "공지 관련 API입니다.")
@@ -22,6 +24,7 @@ import static leets.weeth.domain.event.entity.enums.ResponseMessage.*;
 public class NoticeController {
     private final NoticeService noticeService;
 
+    // 공지사항 생성
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping("/create")
     public CommonResponse<String> createNotice(@RequestBody @Valid RequestNotice requestNotice, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
@@ -29,6 +32,7 @@ public class NoticeController {
         return CommonResponse.createSuccess(EVENT_CREATED_SUCCESS.getMessage());
     }
 
+    // 공지사항 세부 조회
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/{id}")
     public CommonResponse<ResponseNotice> getNotice(@PathVariable Long id) {
@@ -36,6 +40,15 @@ public class NoticeController {
         return CommonResponse.createSuccess(responseNotice);
     }
 
+    // 공지사항 전부
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/")
+    public CommonResponse<List<ResponseNotice>> getAllNotices() {
+        List<ResponseNotice> responseNotices = noticeService.getNotices();
+        return CommonResponse.createSuccess(responseNotices);
+    }
+
+    // 공지사항 수정
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PatchMapping("/{id}")
     public CommonResponse<String> updateNotice(@PathVariable Long id, @RequestBody RequestNotice requestNotice, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
@@ -43,7 +56,7 @@ public class NoticeController {
         return CommonResponse.createSuccess(EVENT_UPDATED_SUCCESS.getMessage());
     }
 
-
+    // 공지사항 삭제
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public CommonResponse<String> deleteNotice(@PathVariable Long id, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
--- src/main/java/leets/weeth/domain/notice/service/NoticeService.java
+++ src/main/java/leets/weeth/domain/notice/service/NoticeService.java
@@ -7,14 +7,20 @@ import leets.weeth.domain.event.entity.enums.Status;
 import leets.weeth.domain.notice.dto.RequestNotice;
 import leets.weeth.domain.notice.dto.ResponseNotice;
 import leets.weeth.domain.event.repository.EventRepository;
+import leets.weeth.domain.notice.mapper.NoticeMapper;
 import leets.weeth.domain.user.entity.User;
 import leets.weeth.domain.user.repository.UserRepository;
 import leets.weeth.global.common.error.exception.custom.BusinessLogicException;
+import leets.weeth.global.common.error.exception.custom.NoticeNotFoundException;
+import leets.weeth.global.common.error.exception.custom.UserNotFoundException;
+import leets.weeth.global.common.error.exception.custom.UserNotMatchException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static leets.weeth.domain.event.entity.enums.ErrorMessage.*;
 
@@ -23,45 +29,38 @@ import static leets.weeth.domain.event.entity.enums.ErrorMessage.*;
 public class NoticeService {
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
-
+    private final NoticeMapper noticeMapper;
 
     // 공지생성
     @Transactional
-    public void createNotice(RequestNotice requestNotice, Long userId) throws BusinessLogicException {
-
-        LocalDateTime start = requestNotice.time();
-        LocalDateTime end = requestNotice.time();
-        RequestEvent requestEvent = RequestEvent.builder()
-                .title(requestNotice.title())
-                .content(requestNotice.content())
-                .startDateTime(start)
-                .endDateTime(end)
-                .location(null)// 장소는 null로 설정
-                .build();
+    public void createNotice(RequestNotice requestNotice, Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND.getMessage()));
+                .orElseThrow(UserNotFoundException::new);
 
-        eventRepository.save(Event.fromDto(requestEvent, Status.NOTICE, user));
+        LocalDateTime now = LocalDateTime.now();
+        eventRepository.save(Event.fromNoticeDto(requestNotice, Status.NOTICE, user, now));
     }
 
+    // 모든 공지사항 조회
+    @Transactional(readOnly = true)
+    public List<ResponseNotice> getNotices(){
+        List<Event> events = eventRepository.findAllByStatus(Status.NOTICE, Sort.by(Sort.Direction.ASC, "id"));
+
+        return events.stream()
+                .map(noticeMapper::toDto)
+                .toList();
+    }
 
     // 공지 상세 조회
     @Transactional(readOnly = true)
     public ResponseNotice getNoticeById(Long id) {
-
         // getNotice로 온 요청엔 공지사항 게시판에서 온 요청이라 가정. NOTICE만 반환
         Event event = eventRepository.findByIdAndStatus(id, Status.NOTICE)
-                .orElseThrow(() -> new EntityNotFoundException(EVENT_NOT_FOUND.getMessage()));
+                .orElseThrow(() -> new EntityNotFoundException(EVENT_NOT_FOUND.getMessage()));//예외 수정 statusNotMatch
 
-        return ResponseNotice.builder()
-                .title(event.getTitle())
-                .content(event.getContent())
-                .created_at(event.getCreatedAt())
-                .modified_at(event.getModifiedAt())
-                .build();
+
+        return noticeMapper.toDto(event);
     }
-
-
 
     // 공지 수정
     @Transactional
@@ -70,24 +69,18 @@ public class NoticeService {
         Event oldEvent = validateEventOwner(eventId, userId);
 
         if(oldEvent.getStatus().equals(Status.NOTICE)) {
-            LocalDateTime start = requestNotice.time();
-            LocalDateTime end = requestNotice.time();
-            RequestEvent updatedEvent = RequestEvent.builder()
-                    .title(requestNotice.title())
-                    .content(requestNotice.content())
-                    .startDateTime(start)
-                    .endDateTime(end)
-                    .location(null)// 장소는 null로 설정
-                    .build();
-            oldEvent.updateFromDto(updatedEvent);
-        } else {//예외처리: 공지사항이 아니라 수정이 안된다
+            LocalDateTime now = LocalDateTime.now();
+
+            oldEvent.updateFromNoticeDto(requestNotice, now);
+        } else {
+            System.out.println("공지사항이 아닙니다");
         }
     }
 
 
     // 공지 삭제
     @Transactional
-    public void deleteNotice(Long noticeId, Long userId) throws BusinessLogicException {
+    public void deleteNotice(Long noticeId, Long userId) throws UserNotMatchException {
         // 공지사항이 맞는지 확인
         Event oldEvent = validateEventOwner(noticeId, userId);
 
@@ -98,16 +91,17 @@ public class NoticeService {
     }
 
     // 해당 일정을 생성한 사용자와 같은지 검증
-    private Event validateEventOwner(Long eventId, Long userId) throws BusinessLogicException {
+    private Event validateEventOwner(Long eventId, Long userId) throws UserNotMatchException {
         Event oldEvent = eventRepository.findById(eventId)
-                .orElseThrow(() -> new EntityNotFoundException(EVENT_NOT_FOUND.getMessage()));
+                .orElseThrow(NoticeNotFoundException::new);
 
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND.getMessage()));
+                .orElseThrow(UserNotFoundException::new);
+        // -> findByIdAndUserId로 변경 / 중복되긴 하는데 이게 의미상 더 맞는 듯
 
         // 일정을 생성한 사용자와 같은지 확인
         if(!user.getId().equals(oldEvent.getUser().getId())){
-            throw new BusinessLogicException(USER_NOT_MATCH.getMessage());
+            throw new UserNotMatchException();
         }
         return oldEvent;
     }
