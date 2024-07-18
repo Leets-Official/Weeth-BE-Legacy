b31d258c2e6cdc5b88ce1ee7a121ca4f3a7aea77
hyxklee <ewgt1234@naver.com>
2024-07-18 18:46:43 +0900

    refactor: 오타수정

--- src/main/java/leets/weeth/global/common/error/exception/custom/TypeNotMatchException.java
+++ src/main/java/leets/weeth/global/common/error/exception/custom/TypeNotMatchException.java
@@ -1,5 +1,5 @@
 package leets.weeth.global.common.error.exception.custom;
 
 public class TypeNotMatchException extends BusinessLogicException{
-    public TypeNotMatchException() {super("타입이 맞지 않습니다.(NOTICE, EVENT, ATTENDANCE");}
+    public TypeNotMatchException() {super("타입이 맞지 않습니다.(NOTICE, EVENT, ATTENDANCE)");}
 }
