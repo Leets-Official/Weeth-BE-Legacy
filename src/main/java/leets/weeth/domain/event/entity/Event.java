0987df1fe9f2cd561f61eb700060bfa269b43195
hyxklee <ewgt1234@naver.com>
2024-07-18 11:37:12 +0900

    feat: 일정, 공지사항을 나눠 메서드 구현

--- src/main/java/leets/weeth/domain/event/entity/Event.java
+++ src/main/java/leets/weeth/domain/event/entity/Event.java
@@ -3,6 +3,7 @@ package leets.weeth.domain.event.entity;
 import jakarta.persistence.*;
 import leets.weeth.domain.event.dto.RequestEvent;
 import leets.weeth.domain.event.entity.enums.Status;
+import leets.weeth.domain.notice.dto.RequestNotice;
 import leets.weeth.domain.user.entity.User;
 import leets.weeth.global.common.entity.BaseEntity;
 import lombok.*;
@@ -43,20 +44,32 @@ public class Event extends BaseEntity {
     private User user;
 
     // 정적 팩토리 메서드
-    public static Event fromDto(RequestEvent dto, Status status, User user) {
+    public static Event fromEventDto(RequestEvent dto, User user) {
         return Event.builder()
                 .title(dto.title())
                 .content(dto.content())
                 .location(dto.location())
                 .startDateTime(dto.startDateTime())
                 .endDateTime(dto.endDateTime())
+                .status(dto.status())
+                .user(user)
+                .build();
+    }
+
+    public static Event fromNoticeDto(RequestNotice dto, Status status, User user, LocalDateTime now) {
+        return Event.builder()
+                .title(dto.title())
+                .content(dto.content())
+                .location(null)
+                .startDateTime(now)
+                .endDateTime(now)
                 .status(status)
                 .user(user)
                 .build();
     }
 
     // 일정 수정을 위한 메소드
-    public void updateFromDto(RequestEvent dto) {
+    public void updateFromEventDto(RequestEvent dto) {
         Optional.ofNullable(dto.title()).ifPresent(title -> this.title = title);
         Optional.ofNullable(dto.content()).ifPresent(content -> this.content = content);
         Optional.ofNullable(dto.location()).ifPresent(location -> this.location = location);
@@ -64,5 +77,13 @@ public class Event extends BaseEntity {
         Optional.ofNullable(dto.endDateTime()).ifPresent(endDateTime -> this.endDateTime = endDateTime);
         Optional.ofNullable(dto.status()).ifPresent(status -> this.status = status);
         // status 수정 공지는 상관없고 출석인지 이벤트인지만
+    }
+
+    // 공지 수정을 위한 메소드
+    public void updateFromNoticeDto(RequestNotice dto, LocalDateTime now) {
+        Optional.ofNullable(dto.title()).ifPresent(title -> this.title = title);
+        Optional.ofNullable(dto.content()).ifPresent(content -> this.content = content);
+        Optional.ofNullable(now).ifPresent(startDateTime -> this.startDateTime = now);
+        Optional.ofNullable(now).ifPresent(endDateTime -> this.endDateTime = now);
     }
 }
