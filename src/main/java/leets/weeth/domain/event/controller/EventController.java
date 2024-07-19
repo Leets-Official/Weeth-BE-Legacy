dd4aea503c7b620775a39d384d45bf13be82f814
hyxklee <ewgt1234@naver.com>
2024-07-20 00:43:54 +0900

    refactor: 파라미터 명 수정

--- src/main/java/leets/weeth/domain/event/controller/EventController.java
+++ src/main/java/leets/weeth/domain/event/controller/EventController.java
@@ -12,7 +12,6 @@ import leets.weeth.global.common.error.exception.custom.BusinessLogicException;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -29,25 +28,21 @@ public class EventController {
     private final EventService eventService;
 
     @Operation(summary = "일정 생성", description = "관리자가 일정을 등록합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping("/create")
-    public CommonResponse<String> createEvent(@RequestBody @Valid RequestEvent requestEvent, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
+    public CommonResponse<String> createEvent(@RequestBody @Valid RequestEvent requestEvent,
+                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
         eventService.createEvent(requestEvent, userId);
         return CommonResponse.createSuccess(EVENT_CREATED_SUCCESS.getMessage());
     }
 
-
     @Operation(summary = "일정 상세 조회", description = "사용자가 일정의 세부사항을 조회합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @GetMapping("/{id}")
-    public CommonResponse<ResponseEvent> getEvent(@PathVariable Long id) {
-        ResponseEvent responseEvent = eventService.getEventById(id);
+    @GetMapping("/{eventId}")
+    public CommonResponse<ResponseEvent> getEvent(@PathVariable Long eventId) {
+        ResponseEvent responseEvent = eventService.getEventById(eventId);
         return CommonResponse.createSuccess(responseEvent);
     }
 
-
     @Operation(summary = "기간 일정 조회", description = "사용자가 일정 기간의 세부사항을 조회합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("")
     public CommonResponse<List<ResponseEvent>> getEvents(
             @RequestParam(name = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
@@ -58,29 +53,25 @@ public class EventController {
     }
 
     @Operation(summary = "년도별 일정 조회", description = "사용자가 1년 단위로 일정을 조회합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @GetMapping("/year") //requestParam으로 해보기
+    @GetMapping("/year")
     public CommonResponse<Map<Integer, List<ResponseEvent>>> getYearEvents(@RequestParam int year) throws BusinessLogicException {
         Map<Integer, List<ResponseEvent>> responseEvents = eventService.getEventsOfYear(year);
         return CommonResponse.createSuccess(responseEvents);
     }
 
-
     @Operation(summary = "일정 수정", description = "관리자가 일정을 수정합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @PatchMapping("/{id}")
-    public CommonResponse<String> updateEvent(@PathVariable Long id, @RequestBody RequestEvent requestEvent, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        eventService.updateEvent(id, requestEvent, userId);
+    @PatchMapping("/{eventId}")
+    public CommonResponse<String> updateEvent(@PathVariable Long eventId, @RequestBody RequestEvent requestEvent,
+                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
+        eventService.updateEvent(eventId, requestEvent, userId);
         return CommonResponse.createSuccess(EVENT_UPDATED_SUCCESS.getMessage());
     }
 
-
     @Operation(summary = "일정 삭제", description = "관리자가 일정을 삭제합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @DeleteMapping("/{id}")
-    public CommonResponse<String> deleteEvent(@PathVariable Long id, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        eventService.deleteEvent(id, userId);
+    @DeleteMapping("/{eventId}")
+    public CommonResponse<String> deleteEvent(@PathVariable Long eventId,
+                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
+        eventService.deleteEvent(eventId, userId);
         return CommonResponse.createSuccess(EVENT_DELETED_SUCCESS.getMessage());
     }
-
 }
