03b6a8c099721c2c2ae87a83e22d4cbc0491b86b
hyxklee <ewgt1234@naver.com>
2024-07-20 16:07:01 +0900

    feat: 출석 일정을 생성 메시지 상수 추가

---
+++ src/main/java/leets/weeth/domain/admin/attendanceEvent/enums/ResponseMessage.java
@@ -0,0 +1,12 @@
+package leets.weeth.domain.admin.attendanceEvent.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ResponseMessage {
+    ATTENDANCE_EVENT_CREATED_SUCCESS("출석일정 생성 성공.");
+
+    private final String message;
+}
