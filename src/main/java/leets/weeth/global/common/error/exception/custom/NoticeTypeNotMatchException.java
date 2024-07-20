2ab92ed96bba51d7b485e684c35dd9ded6eed214
hyxklee <ewgt1234@naver.com>
2024-07-20 16:34:25 +0900

    feat: 타입 매칭 에러 추가

---
+++ src/main/java/leets/weeth/global/common/error/exception/custom/NoticeTypeNotMatchException.java
@@ -0,0 +1,5 @@
+package leets.weeth.global.common.error.exception.custom;
+
+public class NoticeTypeNotMatchException extends TypeNotMatchException{
+    public NoticeTypeNotMatchException() {super("공지사항은 공지사항 게시판에서 수정하세요.");}
+}
