a7e695de84ce8a8b6083c5e551d927cbd4a804c9
hyxklee <ewgt1234@naver.com>
2024-07-21 15:23:30 +0900

    refactor: /admin 권한이 필요한 api는 adminController로 이동

--- src/main/java/leets/weeth/domain/event/controller/EventController.java
+++ src/main/java/leets/weeth/domain/event/controller/EventController.java
@@ -1,13 +1,9 @@
 package leets.weeth.domain.event.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import leets.weeth.domain.event.dto.RequestEvent;
 import leets.weeth.domain.event.dto.ResponseEvent;
 import leets.weeth.domain.event.service.EventService;
-import leets.weeth.global.auth.annotation.CurrentUser;
 import leets.weeth.global.common.error.exception.custom.BusinessLogicException;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,32 +14,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-import static leets.weeth.domain.event.entity.enums.ResponseMessage.*;
-
 @Tag(name = "EventController", description = "일정 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/event")
+@RequestMapping("/event")
 public class EventController {
     private final EventService eventService;
 
-    @Operation(summary = "일정 생성", description = "관리자가 일정을 등록합니다.")
-    @PostMapping("/admin/event/create")
-    public CommonResponse<String> createEvent(@RequestBody @Valid RequestEvent requestEvent,
-                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        eventService.createEvent(requestEvent, userId);
-        return CommonResponse.createSuccess(EVENT_CREATED_SUCCESS.getMessage());
-    }
-
     @Operation(summary = "일정 상세 조회", description = "사용자가 일정의 세부사항을 조회합니다.")
-    @GetMapping("/event/{eventId}")
+    @GetMapping("/{eventId}")
     public CommonResponse<ResponseEvent> getEvent(@PathVariable Long eventId) {
         ResponseEvent responseEvent = eventService.getEventById(eventId);
         return CommonResponse.createSuccess(responseEvent);
     }
 
     @Operation(summary = "기간 일정 조회", description = "사용자가 일정 기간의 세부사항을 조회합니다.")
-    @GetMapping("/event/")
+    @GetMapping("")
     public CommonResponse<List<ResponseEvent>> getEvents(
             @RequestParam(name = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
             @RequestParam(name = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) throws BusinessLogicException {
@@ -53,25 +39,9 @@ public class EventController {
     }
 
     @Operation(summary = "년도별 일정 조회", description = "사용자가 1년 단위로 일정을 조회합니다.")
-    @GetMapping("/event/year")
+    @GetMapping("/year")
     public CommonResponse<Map<Integer, List<ResponseEvent>>> getEventsByYear(@RequestParam int year) throws BusinessLogicException {
         Map<Integer, List<ResponseEvent>> responseEvents = eventService.getEventsByYear(year);
         return CommonResponse.createSuccess(responseEvents);
-    }
-
-    @Operation(summary = "일정 수정", description = "관리자가 일정을 수정합니다.")
-    @PatchMapping("/admin/event/{eventId}")
-    public CommonResponse<String> updateEvent(@PathVariable Long eventId, @RequestBody RequestEvent requestEvent,
-                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        eventService.updateEvent(eventId, requestEvent, userId);
-        return CommonResponse.createSuccess(EVENT_UPDATED_SUCCESS.getMessage());
-    }
-
-    @Operation(summary = "일정 삭제", description = "관리자가 일정을 삭제합니다.")
-    @DeleteMapping("/admin/event/{eventId}")
-    public CommonResponse<String> deleteEvent(@PathVariable Long eventId,
-                                              @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        eventService.deleteEvent(eventId, userId);
-        return CommonResponse.createSuccess(EVENT_DELETED_SUCCESS.getMessage());
     }
 }
--- src/main/java/leets/weeth/domain/notice/controller/NoticeController.java
+++ src/main/java/leets/weeth/domain/notice/controller/NoticeController.java
@@ -1,68 +1,37 @@
 package leets.weeth.domain.notice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import leets.weeth.domain.notice.dto.RequestNotice;
 import leets.weeth.domain.notice.dto.ResponseNotice;
 import leets.weeth.domain.notice.service.NoticeService;
-import leets.weeth.global.auth.annotation.CurrentUser;
-import leets.weeth.global.common.error.exception.custom.BusinessLogicException;
 import leets.weeth.global.common.error.exception.custom.TypeNotMatchException;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import static leets.weeth.domain.notice.enums.ResponseMessage.*;
 
 @Tag(name = "NoticeController", description = "공지 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/notice")
+@RequestMapping("/notice")
 public class NoticeController {
     private final NoticeService noticeService;
 
-    @Operation(summary = "공지 생성", description = "관리자가 공지사항을 등록합니다.")
-    @PostMapping("/admin/notice/create")
-    public CommonResponse<String> createNotice(@RequestPart(value = "requestNotice") @Valid RequestNotice requestNotice,
-                                               @RequestPart(value = "files", required = false) List<MultipartFile> files,
-                                               @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        noticeService.createNotice(requestNotice, files, userId);
-        return CommonResponse.createSuccess(NOTICE_CREATED_SUCCESS.getMessage());
-    }
-
     @Operation(summary = "공지사항 상세 조회", description = "사용자가 공지사항을 조회합니다.")
-    @GetMapping("/notice/{noticeId}")
+    @GetMapping("/{noticeId}")
     public CommonResponse<ResponseNotice> getNotice(@PathVariable Long noticeId) throws TypeNotMatchException {
         ResponseNotice responseNotice = noticeService.getNoticeById(noticeId);
         return CommonResponse.createSuccess(responseNotice);
     }
 
     @Operation(summary = "전체 공지사항 조회", description = "사용자가 전체 공지사항을 조회합니다.")
-    @GetMapping("/notice/")
+    @GetMapping("")
     public CommonResponse<List<ResponseNotice>> getAllNotices() {
         List<ResponseNotice> responseNotices = noticeService.getAllNotices();
         return CommonResponse.createSuccess(responseNotices);
-    }
-
-    @Operation(summary = "공지사항 수정", description = "관리자가 공지사항을 수정합니다.")
-    @PatchMapping("/admin/notice/{noticeId}")
-    public CommonResponse<String> updateNotice(@RequestPart(value = "requestNotice") @Valid RequestNotice requestNotice,
-                                               @RequestPart(value = "files", required = false) List<MultipartFile> files,
-                                               @Parameter(hidden = true) @CurrentUser Long userId,
-                                               @PathVariable Long noticeId) throws BusinessLogicException {
-        noticeService.updateNotice(noticeId, requestNotice, files, userId);
-        return CommonResponse.createSuccess(NOTICE_UPDATED_SUCCESS.getMessage());
-    }
-
-    @Operation(summary = "공지사항 삭제", description = "관리자가 공지사항을 삭제합니다.")
-    @DeleteMapping("/admin/notice/{noticeId}")
-    public CommonResponse<String> deleteNotice(@PathVariable Long noticeId, @Parameter(hidden = true) @CurrentUser Long userId) throws BusinessLogicException {
-        noticeService.deleteNotice(noticeId, userId);
-        return CommonResponse.createSuccess(NOTICE_DELETED_SUCCESS.getMessage());
     }
 }
