b124a822a192c23c53870b3f5bac6df8839d6193
huncozyboy <leejh5838@gmail.com>
2024-07-21 10:47:18 +0900

    Refactor: Spring Security에서 경로 권한 설정

--- src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
+++ src/main/java/leets/weeth/domain/attendance/controller/AttendanceController.java
@@ -7,11 +7,10 @@ import leets.weeth.domain.attendance.dto.ResponseAttendance;
 import leets.weeth.domain.attendance.service.AttendanceService;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import jakarta.validation.Valid;
+
 @Tag(name = "AttendanceController", description = "출석 관련 API")
 @RestController
 @RequestMapping("/attendance")
@@ -21,7 +20,6 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
 
     @Operation(summary = "출석 체크", description = "사용자가 출석 코드를 입력하여 출석합니다.")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping("/check-in")
     public CommonResponse<ResponseAttendance> recordAttendance(
             @RequestBody @Valid RequestAttendance requestDto,
