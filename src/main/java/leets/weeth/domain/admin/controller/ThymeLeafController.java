d2dcda7ff3ee8e79669b8438f707a1e9df681dc7
hyxklee <ewgt1234@naver.com>
2024-07-26 23:57:29 +0900

    feat: 타임리프 경로 컨트롤러 추가

--- src/main/java/leets/weeth/domain/admin/controller/ThymeLeafController.java
+++ src/main/java/leets/weeth/domain/admin/controller/ThymeLeafController.java
@@ -2,8 +2,10 @@ package leets.weeth.domain.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/adminpage")
 public class ThymeLeafController {
 
     @GetMapping("/home")
@@ -11,7 +13,7 @@ public class ThymeLeafController {
         return "home";
     }
 
-    @GetMapping("/login-html")
+    @GetMapping("/login")
     public String login() {
         return "login-html";
     }
@@ -26,9 +28,13 @@ public class ThymeLeafController {
         return "attendance";
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "test";
+    @GetMapping("/penalty")
+    public String penalty() {
+        return "penalty";
     }
 
+    @GetMapping("/account")
+    public String account() {
+        return "account";
+    }
 }
