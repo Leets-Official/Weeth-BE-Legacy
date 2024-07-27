c22bd3b275d217836bc296ba0593cc10bf8a05dc
hyxklee <ewgt1234@naver.com>
2024-07-28 00:11:56 +0900

    feat: week 응답 dto에 date 추가

--- src/main/java/leets/weeth/domain/attendance/dto/ResponseWeekCode.java
+++ src/main/java/leets/weeth/domain/attendance/dto/ResponseWeekCode.java
@@ -3,13 +3,17 @@ package leets.weeth.domain.attendance.dto;
 import leets.weeth.domain.attendance.entity.Week;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class ResponseWeekCode {
     private int weekNumber;
     private String attendanceCode;
+    private LocalDate localDate;
 
     public ResponseWeekCode(Week week) {
         this.weekNumber = week.getWeekNumber();
         this.attendanceCode = week.getAttendanceCode();
+        this.localDate = week.getDate();
     }
 }
