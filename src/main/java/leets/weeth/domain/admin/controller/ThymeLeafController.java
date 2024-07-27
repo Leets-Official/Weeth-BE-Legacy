f6639b10fd6a762c2d7a92ba9ccc094731dab01a
hyxklee <ewgt1234@naver.com>
2024-07-28 02:18:26 +0900

    refactor: 브라우저 탭에 표시되는 정보 수정

--- src/main/java/leets/weeth/domain/admin/controller/ThymeLeafController.java
+++ src/main/java/leets/weeth/domain/admin/controller/ThymeLeafController.java
@@ -1,6 +1,7 @@
 package leets.weeth.domain.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,32 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ThymeLeafController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "weeth admin");
         return "home";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String loginPage(Model model) {
+        model.addAttribute("pageTitle", "Login");
         return "login-html";
     }
 
     @GetMapping("/members")
-    public String members() {
+    public String members(Model model) {
+        model.addAttribute("pageTitle", "멤버 관리");
         return "members";
     }
 
     @GetMapping("/attendance")
-    public String attendance() {
+    public String attendance(Model model) {
+        model.addAttribute("pageTitle", "출석 관리");
         return "attendance";
     }
 
     @GetMapping("/penalty")
-    public String penalty() {
+    public String penalty(Model model) {
+        model.addAttribute("pageTitle", "패널티 관리");
         return "penalty";
     }
 
     @GetMapping("/account")
-    public String account() {
+    public String account(Model model) {
+        model.addAttribute("pageTitle", "weeth admin 회비");
         return "account";
     }
 }
