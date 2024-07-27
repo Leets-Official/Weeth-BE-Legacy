8deafe495944b8ecae9338171452da15ec3c0952
hyxklee <ewgt1234@naver.com>
2024-07-28 00:13:30 +0900

    feat: 관리자가 출석 일정과 code를 한 번에 볼 수 있도록 수정

--- src/main/java/leets/weeth/domain/event/attendanceEvent/service/AttendanceEventService.java
+++ src/main/java/leets/weeth/domain/event/attendanceEvent/service/AttendanceEventService.java
@@ -1,20 +1,32 @@
 package leets.weeth.domain.event.attendanceEvent.service;
 
+import leets.weeth.domain.attendance.dto.ResponseWeekCode;
+import leets.weeth.domain.attendance.entity.Week;
+import leets.weeth.domain.attendance.repository.WeekRepository;
 import leets.weeth.domain.event.attendanceEvent.dto.RequestAttendanceEvent;
+import leets.weeth.domain.event.attendanceEvent.dto.ResponseAttendanceEvent;
 import leets.weeth.domain.event.attendanceEvent.mapper.AttendanceEventMapper;
+import leets.weeth.domain.event.entity.Event;
+import leets.weeth.domain.event.entity.enums.Type;
 import leets.weeth.domain.event.repository.EventRepository;
 import leets.weeth.domain.user.entity.User;
 import leets.weeth.domain.user.repository.UserRepository;
 import leets.weeth.global.common.error.exception.custom.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AttendanceEventService {
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
+    private final WeekRepository weekRepository;
     private final AttendanceEventMapper mapper;
 
     /*
@@ -31,5 +43,23 @@ public class AttendanceEventService {
                 .orElseThrow(UserNotFoundException::new);
 
         eventRepository.save(mapper.fromAttendanceEventDto(requestDto, user));
+    }
+
+    // 출석 일정 + Code 한 번에 조회
+    @Transactional(readOnly = true)
+    public List<ResponseAttendanceEvent> getAttendanceEvents() {
+        List<Event> events = eventRepository.findAllByType(Type.ATTENDANCE, Sort.by(Sort.Direction.ASC, "startDateTime"));
+        List<Week> weeks = weekRepository.findAll();
+
+        Map<LocalDate, Week> weekMap = weeks.stream()
+                .collect(Collectors.toMap(Week::getDate, week -> week));
+
+        return events.stream()
+                .map(event -> {
+                    Week matchingWeek = weekMap.get(event.getStartDateTime().toLocalDate());
+                    return mapper.toAttendanceEventDto(event, matchingWeek);
+                })
+                .filter(Objects::nonNull)
+                .toList();
     }
 }
